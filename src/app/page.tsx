b928084579f79e88a351d9445c319fe26cbc4579
Ajar Dahal <ajardahal@gmail.com>
2026-01-19 13:32:22 -0800

    set up header for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
 "use client";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
 
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignUpButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-  
-  <div>
-    <h1>Home Page</h1>
-    <SignedOut>
-      <SignUpButton mode="modal">
-        <Button>Sign Up</Button>
-      </SignUpButton>
-    </SignedOut>
+  <div className="min-h-screen bg-background">
+    <Header />
+    <Hero/>
+    <HowItWorks />
+    <WhatToAsk />
+    <PricingSection />
+    <CTA />
+    <Footer />
 
-    <SignedIn>
-      <SignOutButton>
-        <Button>Logout</Button>
-      </SignOutButton>
-    </SignedIn>
   </div>
   );
 }
